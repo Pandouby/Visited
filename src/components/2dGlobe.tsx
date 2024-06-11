cac510ba3df364e3c2499e0046071f4350ccd573
Silvan Dubach <silvan.dubach@ti8m.ch>
2024-06-11 16:35:33 +0200

    Add countour point interpolation, add curvature to globe polygon

--- src/components/2dGlobe.tsx
+++ src/components/2dGlobe.tsx
@@ -1,23 +1,18 @@
 import { Line, Preload } from "@react-three/drei/native";
 import { useLoader } from "@react-three/fiber/native";
+import { FeatureCollection } from "geojson";
 import React, { Suspense, useMemo } from "react";
-import * as THREE from "three";
-import {
-    Texture,
-    TextureLoader,
-    Vector2,
-    Vector3
-} from "three";
-import countriesJson from "../data/low-res.geo.json";
-
-interface ICountry {
-    sovereignt: string;
-    sovereigntShort: string;
-    coordinates: Vector3[][];
-}
+import { AdditiveBlending, Texture, TextureLoader } from "three";
+import { GenerateCountryPolygon } from "../GenerateCountryPolygon";
+import countriesJsonData from "../data/low-res.geo.json";
 
 export const GlobeMap = () => {
-  const radiusGlobe = 4;
+  // Constants for globe calculation
+  const globeRadius = 4;
+  const polygonResolution = 5; // In angular degrees
+
+  // Data / Texture import
+  const countriesJson = countriesJsonData as FeatureCollection;
 
   const earthTexture: Texture = useLoader(
     TextureLoader,
@@ -32,118 +27,110 @@ export const GlobeMap = () => {
   const cloudTexture: Texture = useLoader(
     TextureLoader,
     require("../../assets/cloud.jpeg")
-  ) as Texture;  
+  ) as Texture;
 
-  const convertCartesian = (point) => {
-    const lambda = (point[0] * Math.PI) / 180;
-    const phi = (point[1] * Math.PI) / 180;
-    const cosPhi = Math.cos(phi);
-    return new Vector3(
-        // Remove comment brackets to lift countries so they dont clip the earth
-      (radiusGlobe /*+ 0.051*/) * cosPhi * Math.cos(lambda),
-      (radiusGlobe /*+ 0.051*/) * Math.sin(phi),
-      -(radiusGlobe /*+ 0.051*/) * cosPhi * Math.sin(lambda),
-    );
-  };
-
-  const maptomap = (point) => {
-    return new Vector2(
-     point[0],
-     point[1],
-    );
-  };
-
-  const countries = useMemo(
+  const polygonCountries = useMemo(
     () =>
       countriesJson.features.map((country) => {
         if (country.geometry.type === "Polygon") {
-          return country.geometry.coordinates[0].map(maptomap);
+          return [
+            GenerateCountryPolygon(
+              country.geometry.coordinates,
+              polygonResolution,
+              globeRadius + 0.01 // + 0.01 to avoid clipping with globe texture
+            ),
+          ];
         } else if (country.geometry.type === "MultiPolygon") {
-          return country.geometry.coordinates.flatMap((coordinates) => 
-             coordinates.map((polygon) =>  polygon.map(maptomap))
-        );
+          const multiPoly = [];
+          country.geometry.coordinates.map((coordinate) => {
+            if (coordinate[0].length > 40) {
+              multiPoly.push(
+                GenerateCountryPolygon(
+                  coordinate,
+                  polygonResolution,
+                  globeRadius + 0.01 // + 0.01 to avoid clipping with globe texture
+                )
+              );
+            }
+          });
+          return multiPoly;
         }
         return [];
       }),
     []
   );
 
-  const countriesFinal = [];
-  countries.map((country) => {
-    if(Array.isArray(country[0])) {
-        country.map((test) => {
-            if(test.length < 50) return;
-            const polyShape = new THREE.Shape(
-                test.map((coord) => new THREE.Vector2(coord.x, coord.y))
-              );
-              const polyGeometry = new THREE.ShapeGeometry(polyShape);
-              polyGeometry.setAttribute(
-                "position",
-                new THREE.Float32BufferAttribute(
-                    test.map((coord) => [coord.x, coord.y]).flat(),
-                  2
-                )
-              );
-              countriesFinal.push(polyGeometry);
-        })
-    } else {
-        const polyShape = new THREE.Shape(
-            country.map((coord) => new THREE.Vector2(coord.x, coord.y))
-          );
-          const polyGeometry = new THREE.ShapeGeometry(polyShape);
-          polyGeometry.setAttribute(
-            "position",
-            new THREE.Float32BufferAttribute(
-              country.map((coord) => [coord.x, coord.y]).flat(),
-              2
-            )
-          );
-          countriesFinal.push(polyGeometry);
-    }
-  });
-
-  console.log(countriesFinal.length);
-
   return (
     <Suspense fallback={null}>
       <mesh name="lighting">
         {/* <hemisphereLight intensity={10} /> */}
-        <directionalLight color={"#ffffff"} intensity={10} position={[-2, -0.5, 1]}/>
+        <directionalLight
+          color={"#ffffff"}
+          intensity={10}
+          position={[-2, -0.5, 1]}
+        />
       </mesh>
-{/* 
+
       <mesh name="atmosphere" visible position={[0, 0, 0]} rotation={[0, 0, 0]}>
         <sphereGeometry args={[4.2, 64, 64]} />
         <meshStandardMaterial color={"#349eeb"} opacity={0.3} transparent />
       </mesh>
       <mesh name="clouds" visible position={[0, 0, 0]} rotation={[0, 0, 0]}>
         <sphereGeometry args={[4.1, 64, 64]} />
-        <meshStandardMaterial map={cloudTexture} opacity={1} transparent/>
+        <meshStandardMaterial map={cloudTexture} opacity={1} transparent />
       </mesh>
+
       <mesh name="earth-day" visible position={[0, 0, 0]} rotation={[0, 0, 0]}>
         <sphereGeometry args={[4, 64, 64]} />
         <meshStandardMaterial map={earthTexture} opacity={1} />
-      </mesh> */}
+      </mesh>
+      <mesh name="earth-night">
+        <sphereGeometry args={[4, 64, 64]} />
+        <meshBasicMaterial
+          map={earthNightTexture}
+          color={"#ffffff"}
+          blending={AdditiveBlending}
+        />
+      </mesh>
 
-        {countriesFinal.map((country, countryIndex) => (
+      {polygonCountries.map((country, countryIndex) => (
         <mesh
-          key={countryIndex}
-          onClick={() => {
-            console.log("click" + countriesJson.features[countryIndex].properties.sovereignt);
-          }}>
-          {Array.isArray(country[0]) ? (
-            country.map((polygon, polyIndex) => (
-              <mesh key={polyIndex} geometry={polygon}>
-                <meshBasicMaterial color={"red"} wireframe/>
-              </mesh>
-            ))
-          ) : (
-            <mesh key={"asdas"} geometry={country}>
-              <meshBasicMaterial color={"red"} wireframe/>
-            </mesh>
+          onClick={() =>
+            console.log(
+              countriesJson.features[countryIndex].properties.sovereignt
+            )
+          }>
+          {country.map(
+            (polygon) => (
+              console.log(polygon.contour),
+              (
+                <mesh geometry={polygon.bufferGeometry}>
+                  <meshBasicMaterial color={"red"} opacity={0.4} />
+                  {polygon.contour.map((contourPoint) => {
+                    <Line points={contourPoint} />;
+                  })}
+                </mesh>
+              )
+            )
           )}
         </mesh>
       ))}
 
+      {/* 
+      <mesh name="country-borders">
+        {countries.map((country, countryIndex) => (
+            <mesh key={countryIndex} onClick={() => { console.log('click country'); }}>
+                    {Array.isArray(country[0]) ? country.map((polygon, polyIndex) => (
+                        <Line key={polyIndex} points={polygon} color={0x00ff00} opacity={1} />
+                    )) : (
+                        <Line points={country} color={0x00ff00} opacity={1} />
+                )}
+            </mesh>
+
+            
+        ))}
+      </mesh> 
+      */}
 
       <Preload all />
     </Suspense>
--- src/components/Globe.tsx
+++ src/components/Globe.tsx
@@ -1,19 +1,14 @@
-import { Preload, Line} from "@react-three/drei/native";
+import { Preload, Line } from "@react-three/drei/native";
 import { useLoader } from "@react-three/fiber/native";
 import React, { Suspense, useMemo } from "react";
 import * as THREE from "three";
-import {
-    Texture,
-    TextureLoader,
-    Vector2,
-    Vector3
-} from "three";
+import { Texture, TextureLoader, Vector2, Vector3 } from "three";
 import countriesJson from "../data/low-res.geo.json";
 
 interface ICountry {
-    sovereignt: string;
-    sovereigntShort: string;
-    coordinates: Vector3[][];
+  sovereignt: string;
+  sovereigntShort: string;
+  coordinates: Vector3[][];
 }
 
 export const Globe = () => {
@@ -34,19 +29,22 @@ export const Globe = () => {
     require("../../assets/cloud.jpeg")
   ) as Texture;
 
-  const test: ICountry = {sovereignt: "test", sovereigntShort: "tes", coordinates: [[new Vector3(2, 2, 3)]]}
+  const test: ICountry = {
+    sovereignt: "test",
+    sovereigntShort: "tes",
+    coordinates: [[new Vector3(2, 2, 3)]],
+  };
   console.log(test);
-  
 
   const convertCartesian = (point) => {
     const lambda = (point[0] * Math.PI) / 180;
     const phi = (point[1] * Math.PI) / 180;
     const cosPhi = Math.cos(phi);
     return new Vector3(
-        // Remove comment brackets to lift countries so they dont clip the earth
-      (radiusGlobe /*+ 0.051*/) * cosPhi * Math.cos(lambda),
-      (radiusGlobe /*+ 0.051*/) * Math.sin(phi),
-      -(radiusGlobe /*+ 0.051*/) * cosPhi * Math.sin(lambda),
+      // Remove comment brackets to lift countries so they dont clip the earth
+      radiusGlobe /*+ 0.051*/ * cosPhi * Math.cos(lambda),
+      radiusGlobe /*+ 0.051*/ * Math.sin(phi),
+      -(radiusGlobe /*+ 0.051*/) * cosPhi * Math.sin(lambda)
     );
   };
 
@@ -67,36 +65,36 @@ export const Globe = () => {
 
   const countriesFinal = [];
   countries.map((country) => {
-    if(Array.isArray(country[0])) {
-        country.map((test) => {
-            if(test.length < 50) return;
-            const polyShape = new THREE.Shape(
-                test.map((coord) => new THREE.Vector2(coord.x, coord.y))
-              );
-              const polyGeometry = new THREE.ShapeGeometry(polyShape);
-              polyGeometry.setAttribute(
-                "position",
-                new THREE.Float32BufferAttribute(
-                    test.map((coord) => [coord.x, coord.y, coord.z]).flat(),
-                  3
-                )
-              );
-              countriesFinal.push(polyGeometry);
-        })
-    } else {
+    if (Array.isArray(country[0])) {
+      country.map((test) => {
+        if (test.length < 50) return;
         const polyShape = new THREE.Shape(
-            country.map((coord) => new THREE.Vector2(coord.x, coord.y))
-          );
-          const polyGeometry = new THREE.ShapeGeometry(polyShape);
-          polyGeometry.computeVertexNormals();
-          polyGeometry.setAttribute(
-            "position",
-            new THREE.Float32BufferAttribute(
-              country.map((coord) => [coord.x, coord.y, coord.z]).flat(),
-              3
-            )
-          );
-          countriesFinal.push(polyGeometry);
+          test.map((coord) => new THREE.Vector2(coord.x, coord.y))
+        );
+        const polyGeometry = new THREE.ShapeGeometry(polyShape);
+        polyGeometry.setAttribute(
+          "position",
+          new THREE.Float32BufferAttribute(
+            test.map((coord) => [coord.x, coord.y, coord.z]).flat(),
+            3
+          )
+        );
+        countriesFinal.push(polyGeometry);
+      });
+    } else {
+      const polyShape = new THREE.Shape(
+        country.map((coord) => new THREE.Vector2(coord.x, coord.y))
+      );
+      const polyGeometry = new THREE.ShapeGeometry(polyShape);
+      polyGeometry.computeVertexNormals();
+      polyGeometry.setAttribute(
+        "position",
+        new THREE.Float32BufferAttribute(
+          country.map((coord) => [coord.x, coord.y, coord.z]).flat(),
+          3
+        )
+      );
+      countriesFinal.push(polyGeometry);
     }
   });
 
@@ -106,9 +104,13 @@ export const Globe = () => {
     <Suspense fallback={null}>
       <mesh name="lighting">
         {/* <hemisphereLight intensity={10} /> */}
-        <directionalLight color={"#ffffff"} intensity={10} position={[-2, -0.5, 1]}/>
+        <directionalLight
+          color={"#ffffff"}
+          intensity={10}
+          position={[-2, -0.5, 1]}
+        />
       </mesh>
-{/* 
+      {/* 
       <mesh name="atmosphere" visible position={[0, 0, 0]} rotation={[0, 0, 0]}>
         <sphereGeometry args={[4.2, 64, 64]} />
         <meshStandardMaterial color={"#349eeb"} opacity={0.3} transparent />
@@ -127,11 +129,14 @@ export const Globe = () => {
         <meshBasicMaterial map={earthNightTexture} color={"#ffffff"} blending={THREE.AdditiveBlending}/>
       </mesh> */}
 
-        {countriesFinal.map((country, countryIndex) => (
+      {countriesFinal.map((country, countryIndex) => (
         <mesh
           key={countryIndex}
           onClick={() => {
-            console.log("click" + countriesJson.features[countryIndex].properties.sovereignt);
+            console.log(
+              "click" +
+                countriesJson.features[countryIndex].properties.sovereignt
+            );
           }}>
           {Array.isArray(country[0]) ? (
             country.map((polygon, polyIndex) => (
@@ -147,7 +152,7 @@ export const Globe = () => {
         </mesh>
       ))}
 
-    {/* <mesh name="country-borders">
+      {/* <mesh name="country-borders">
         {countries.map((country, countryIndex) => (
             <mesh key={countryIndex} onClick={() => { console.log('click country'); }}>
                     {Array.isArray(country[0]) ? country.map((polygon, polyIndex) => (
